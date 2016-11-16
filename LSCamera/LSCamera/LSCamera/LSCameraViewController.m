bab8e26d5c5eadbf96b5727136231dc12534ccac
lsrain <315192042@qq.com>
2016-11-16 22:29:13 +0800

    添加快门动画

--- LSCamera/LSCamera/LSCamera/LSCameraViewController.m
+++ LSCamera/LSCamera/LSCamera/LSCameraViewController.m
@@ -24,6 +24,29 @@
 
 - (IBAction)clickBtnAction:(UIButton *)sender {
 
+    //关闭快门
+    [self maskViewAminWithClose:YES];
+    
+    // 开启快门
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        
+        [self maskViewAminWithClose:NO];
+    });
+    
+}
+
+- (void)maskViewAminWithClose:(Boolean)close{
+    if (close) {
+        // 禁用选中的约束
+        // IOS 10
+        [NSLayoutConstraint deactivateConstraints:_maskViewConstraint];
+    } else{
+        [NSLayoutConstraint activateConstraints:_maskViewConstraint];
+    }
+    
+    [UIView animateWithDuration:.2 animations:^{
+        [self.view layoutIfNeeded];
+    }];
 }
 
 - (void)didReceiveMemoryWarning {
