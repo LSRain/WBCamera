69824c123afde8b0dfcab1a725be2f3f864e9ea4
lsrain <315192042@qq.com>
2016-11-16 20:31:39 +0800

    布局按钮及添加bundle配图

--- LSCamera/LSCamera/ViewController.m
+++ LSCamera/LSCamera/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LSCameraViewController.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview:[LSCameraViewController new].view];
 }
 
 
