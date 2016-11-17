caccc7146166d55be7666840a7ee24f4431a704a
lsrain <315192042@qq.com>
2016-11-17 08:50:51 +0800

    添加相机快门遮罩视图

---
+++ LSCamera/LSCamera/LSCamera/LSSportCameraMaskView.h
@@ -0,0 +1,16 @@
+//
+//  LSSportCameraMaskView.h
+//  LSCamera
+//
+//  Created by WangBiao on 2016/11/17.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ 相机快门遮罩视图
+ */
+@interface LSSportCameraMaskView : UIView
+
+@end
---
+++ LSCamera/LSCamera/LSCamera/LSSportCameraMaskView.m
@@ -0,0 +1,15 @@
+//
+//  LSSportCameraMaskView.m
+//  LSCamera
+//
+//  Created by WangBiao on 2016/11/17.
+//  Copyright © 2016年 lsrain. All rights reserved.
+//
+
+#import "LSSportCameraMaskView.h"
+
+@implementation LSSportCameraMaskView
+
+
+
+@end
